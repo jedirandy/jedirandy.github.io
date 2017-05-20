1750a28fc3451a5d2739c36a981f05f827484043
Sheng Ran <s.ran@criteo.com>
2017-05-20 10:46:25 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+sed -i '' -e 's/^\/build/#\/build/g' .gitignore
+yarn build
+git add build/
+git commit -m 'deploy'
+git subtree split --prefix build/ --branch build
+git checkout .gitignore
+git checkout build
+git push origin HEAD:master -f
+git checkout src
+git reset --hard HEAD~
