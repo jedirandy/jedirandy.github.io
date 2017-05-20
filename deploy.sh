25c96ec09938b5194a1f4e8a957fbf7a4ace1f42
Sheng Ran <s.ran@criteo.com>
2017-05-20 15:29:17 +0200

    fix the deployment script
    - remove the build branch first

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ sed -i '' -e 's/^\/build/#\/build/g' .gitignore
 yarn build
 git add build/
 git commit -m 'deploy'
+git branch -D build
 git subtree split --prefix build/ --branch build
 git checkout .gitignore
 git checkout build
