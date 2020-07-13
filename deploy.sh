ac611edd80e18c8e640bd43ab9c2e75da004a802
Sheng Ran <jedirandy@gmail.com>
2020-07-14 00:37:10 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@ set -e
 
 hash=$(git subtree split --prefix dist)
 echo "Publishing ${hash}"
+echo "Commit content"
+git log --abbrev-commit --pretty=oneline a96c9269f66080379040c5ca216697b712815011 | head -1
 git push origin ${hash}:master
