bade58e26798e55afc6442fdd40e876d58d777ce
Sheng Ran <jedirandy@gmail.com>
2020-07-15 17:28:27 +0100

    Update help
    - change command text color

--- src/bin/help.ts
+++ src/bin/help.ts
@@ -1,16 +1,17 @@
 import { Terminal } from 'xterm'
 import { Program } from '../types'
 import { writeln } from '../utils'
+import { chalk } from '../global'
 
 
 const help: Program = async (term: Terminal, ...args: string[]) => {
-    await writeln(term, '- about')
-    await writeln(term, `  Shows information about the author`)
-    await writeln(term, '- weather [optional location]')
-    await writeln(term, `  Shows the weather for the location, example: weather svalbard`)
-    await writeln(term, '- fortune')
+    await writeln(term, chalk.yellowBright('> about'))
+    await writeln(term, `  About the author`)
+    await writeln(term, chalk.yellowBright('> weather [optional location]'))
+    await writeln(term, `  Shows the weather for the location, for example: weather svalbard`)
+    await writeln(term, chalk.yellowBright('> fortune'))
     await writeln(term, `  Prints a random quote`)
-    await writeln(term, '- clear')
+    await writeln(term, chalk.yellowBright('> clear'))
     await writeln(term, `  Clears the terminal, ctrl + l also works`)
     return 0
 }
